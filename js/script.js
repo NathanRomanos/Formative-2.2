b699db1a4eb3514ae4fd83c7f379100bb477ec65
Nathan michael glenn Romanos <932400754@bun2-206-07.local>
2019-11-01 15:25:28 +1300

    started adding content

---
+++ js/script.js
@@ -0,0 +1,153 @@
+
+var jackets [
+  {
+    name : "Air Force Bomber Jacket",
+    id : "j101",
+    photo : "/images/airForce.jpeg",
+    type : "Bomber Jacket",
+    size : "M",
+    colour : "olive green",
+    join : "button",
+    para : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name : "Acne Studios Leather Jacket",
+    id : "j102",
+    photo : "/images/leather.jpg",
+    type : "Leather Jacket",
+    size : "L",
+    colour : "brown",
+    join : "zip",
+    para : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name : "Gloverall Duffle Coat",
+    id : "j103",
+    photo : "/images/duffle.jpg",
+    type : "Duffle Coat",
+    size : "L",
+    colour : "dark grey",
+    join : "toggle",
+    para : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  },
+  {
+    name : "",
+    id : "",
+    photo : "/images/.jpg",
+    type : "",
+    size : "",
+    colour : "",
+    join : "",
+    para : ""
+  }
+];
