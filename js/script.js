5d4020a7e2377e9811b8e7122154ee41568a1aa7
Nathan michael glenn Romanos <932400754@bun2-206-07.local>
2019-11-04 15:24:20 +1300

    second change

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
-var jackets [
+var jackets = [
   {
     name : "Air Force Bomber Jacket",
     id : "j101",
     photo : "/images/airForce.jpeg",
-    type : "Bomber Jacket",
+    type : "Bomber jacket",
     size : "M",
     colour : "olive green",
     join : "button",
@@ -14,7 +14,7 @@ var jackets [
     name : "Acne Studios Leather Jacket",
     id : "j102",
     photo : "/images/leather.jpg",
-    type : "Leather Jacket",
+    type : "Leather jacket",
     size : "L",
     colour : "brown",
     join : "zip",
@@ -24,21 +24,21 @@ var jackets [
     name : "Gloverall Duffle Coat",
     id : "j103",
     photo : "/images/duffle.jpg",
-    type : "Duffle Coat",
+    type : "Duffle coat",
     size : "L",
     colour : "dark grey",
     join : "toggle",
     para : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    name : "",
-    id : "",
-    photo : "/images/.jpg",
-    type : "",
-    size : "",
-    colour : "",
-    join : "",
-    para : ""
+    name : "Ecocult Vintage Leather Jacket",
+    id : "j104",
+    photo : "/images/vintageLeather.jpg",
+    type : "Leather jacket",
+    size : "S",
+    colour : "red",
+    join : "zip",
+    para : "This one kinda ugly tho."
   },
   {
     name : "",
@@ -151,3 +151,5 @@ var jackets [
     para : ""
   }
 ];
+
+console.log(jackets)
